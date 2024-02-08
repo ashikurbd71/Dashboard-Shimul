fd7aa90adcf3d66ef3cfd5b1f55cf5d7b7092b0c
ashikurbd71 <ashikurovi2003@gmail.com>
2024-02-08 19:42:16 +0600

    setup project

---
+++ src/Layout/DBlayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DBlayout = () => {
+    return (
+        <div >
+            <h1 className='text-[18px]'>hello</h1>
+        </div>
+    );
+};
+
+export default DBlayout;
